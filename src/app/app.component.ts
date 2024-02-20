cd723b2775974904ed399176c4d470cc43b1844d
EvanBradyS00238375 <S00238375@atu.ie>
2024-02-20 16:52:10 +0000

    1st commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MovieService } from './movie.service';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'week3';
+
+  movies:any[]=[];
+
+  constructor(private movieService:MovieService) {}
+
+  ngOnInit() {
+    this.movies=this.movieService.getMovies();
+    console.log(this.movies);
+  }
+
+  
+
+  
+
+  
+}
